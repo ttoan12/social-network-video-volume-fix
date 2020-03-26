787d42a86b9e7d2cc5076f0aa0653054beeee444
Peter Pshenichny <peter.pshenichny@gmail.com>
2020-03-26 21:49:58 +0200

    functions moved to external files

---
+++ src/lib/misc.ts
@@ -0,0 +1,86 @@
+import { playCallback } from './video';
+
+export function log(...args: any[]) {
+  // @ts-ignore
+  DEBUG
+  && console.log.apply(null, args);
+}
+
+export function keyupCallback(event: KeyboardEvent) {
+  if (['ArrowDown', 'ArrowUp'].find((k) => k === event.key)) {
+    // Fix native facebook scroll when changing vol.
+    event.preventDefault();
+  }
+}
+
+export function pageKeyupCallback(event: KeyboardEvent) {
+  if (!(event.ctrlKey && event.shiftKey && ['ArrowDown', 'ArrowDown', 'Backspace'].find(k => event.key))) {
+    return;
+  }
+
+  chrome.storage.sync.get('volume', (r) => {
+    let update = {};
+    switch (event.key) {
+      case 'Backspace': // Toggle muted
+        update = {...update, muted: !r.muted};
+        break;
+      case 'ArrowDown':
+      case 'ArrowUp':
+        // Calculate new volume level
+        let volume = r.volume + 0.1 * ('ArrowDown' === event.key ? -1 : 1);
+        volume = (volume > 1) ? 1 : volume;
+        volume = (volume < 0) ? 0 : volume;
+        if (volume !== r.volume || r.muted) { // Update only if changed or was muted
+          update = {...update, volume: volume, muted: false};
+        }
+    }
+    if (Object.keys(update).length) {
+      chrome.storage.sync.set({...r, ...update}, () => log('Volume UPDATED to', update));
+    }
+  });
+}
+
+export function extractVideosFromNode(node: Node): Array<HTMLVideoElement> {
+  const videoElements: Array<HTMLVideoElement> = [];
+  // Only Element node allowed
+  if (1 !== node.nodeType) {
+    return videoElements;
+  }
+
+  let videoCollection = (<Element>node).getElementsByTagName('video');
+
+  for (let i = 0; i < videoCollection.length; i++) {
+    videoElements.push(videoCollection.item(i));
+  }
+  return videoElements;
+}
+
+export function appendVideoFunctionality(video: HTMLVideoElement) {
+  video.addEventListener('play', playCallback);
+
+  // Parent container
+  const parentContainer = video.closest('.mtm');
+  if (parentContainer) {
+    log('found video parent container', parentContainer, video);
+    parentContainer.addEventListener('keyup', keyupCallback);
+  }
+}
+
+export function observeContainer(cnt: HTMLElement, cfg: MutationObserverInit) {
+  log('STARTING NEW CONTAINER OBSERVER');
+  return new MutationObserver((mutationsList, obs) => {
+    if (!document.contains(cnt)) {
+      log('LOST CONTAINER');
+      // Node was changed, find new one
+      obs.disconnect();
+      return;
+    }
+
+    // Callback function to execute when mutations are observed
+    for (let mutation of mutationsList) {
+      mutation.addedNodes.forEach((node) => {
+        extractVideosFromNode(node).forEach(appendVideoFunctionality);
+      });
+    }
+  }).observe(cnt, cfg);
+}
---
+++ src/lib/video.ts
@@ -0,0 +1,72 @@
+import {
+  keyupCallback,
+  log,
+} from './misc';
+
+/**
+ * Start listen to global volume change combination
+ *
+ * @param video
+ */
+const getStorageListenerCallback = (video: HTMLVideoElement) => (changes: { [key: string]: { newValue?: any; oldValue?: any; } }) => {
+  if (changes['volume']) {
+    video.volume = changes['volume'].newValue;
+  }
+
+  if (changes['muted']) {
+    video.muted = changes['muted'].newValue;
+  }
+};
+
+/**
+ *
+ * @param pauseEvent
+ */
+function pauseListenerCallback(pauseEvent: Event) {
+  log('PAUSE');
+  chrome.storage.onChanged.removeListener(getStorageListenerCallback(<HTMLVideoElement>pauseEvent.target));
+  pauseEvent.target.removeEventListener('pause', pauseListenerCallback);
+  pauseEvent.target.removeEventListener('volumechange', volumeChangeCallback);
+}
+
+/**
+ *
+ * @param event
+ */
+export function playCallback(event: Event) {
+  log('PLAY');
+  const video: HTMLVideoElement = <HTMLVideoElement>event.target;
+
+  // Parent container
+  const parentContainer = video.closest('.mtm');
+  if (parentContainer) {
+    log('found video parent container', parentContainer, video);
+    parentContainer.addEventListener('keyup', keyupCallback);
+  }
+
+  event.target.addEventListener('pause', pauseListenerCallback);
+  chrome.storage.onChanged.addListener(getStorageListenerCallback(video));
+  event.target.addEventListener('volumechange', volumeChangeCallback);
+
+  chrome.storage.sync.get(['volume', 'muted'], (r) => {
+    if ('undefined' !== typeof r.volume && r.volume !== video.volume) {
+      video.volume = r.volume;
+      video.muted = r.muted;
+    }
+  });
+}
+
+function volumeChangeCallback(event: Event) {
+  log('Volume CHANGE STARTED');
+  const video: HTMLVideoElement = <HTMLVideoElement>event.target;
+
+  // Update only when changed
+  chrome.storage.sync.get(['volume', 'muted'], (r) => {
+    if (r.volume === video.volume && r.muted === video.muted) {
+      return;
+    }
+    chrome.storage.sync.set({...r, volume: video.volume, muted: video.muted}, () => {
+      log('Volume CHANGED to', video.volume, video.muted);
+    });
+  });
+}
